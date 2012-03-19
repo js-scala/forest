20ca9fd26fad0b063fcf06581652e399e31403a6
Julien Richard-Foy <julien@richard-foy.fr>
2012-03-19 10:39:59 +0100

    fix build

--- project/build.scala
+++ project/build.scala
@@ -3,6 +3,4 @@ import Keys._
 
 object Forest extends Build {
   lazy val compiler = Project(id = "forest-compiler", base = file("compiler"))
-  lazy val playRuntime = Project(id = "forest-play-runtime", base = file("play-runtime")) dependsOn compiler
-  lazy val root = Project(id = "forest", base = file(".")) aggregate (compiler, playRuntime)
 }
