0c478c57dbd4b18fd44520d061779c43d8b93ddc
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-03 16:17:26 +0200

    clean up and complete code generation

--- lms/src/main/scala/forest/lms/ForestCodegen.scala
+++ lms/src/main/scala/forest/lms/ForestCodegen.scala
@@ -4,13 +4,27 @@ import virtualization.lms.common._
 import js._
 import java.io.PrintWriter
 
-trait ForestJSCodegen extends JSCodegen {
+trait ForestJSCodegen extends JSGenBase {
   val IR: ForestExp
   import IR._
 
   override def emitNode(sym: Sym[Any], node: Def[Any])(implicit stream: PrintWriter): Unit = node match {
-    case Tag(name, attrs) => {
-      emitValDef(sym, "document.createElement('%s')".format(name))
+    case Tag(name, children, attrs) => {
+      // Create the element
+      emitValDef(sym, s"document.createElement('${name}');")
+      // Add its attributes
+      for ((name, value) <- attrs) {
+        val v = if (value.isEmpty) {
+          "'${name}'"
+        } else {
+          value.map(quote).mkString("+")
+        }
+        stream.append(s"${quote(sym)}.setAttribute('${name}', ${v});"+"\n")
+      }
+      // Append its children nodes
+      for (child <- children) {
+        stream.append(s"${quote(sym)}.appendChild(${quote(child)});"+"\n")
+      }
     }
   }
 }
@@ -20,8 +34,26 @@ trait ForestScalaCodegen extends ScalaGenBase {
   import IR._
 
   override def emitNode(sym: Sym[Any], node: Def[Any])(implicit stream: PrintWriter): Unit = node match {
-    case Tag(name, attrs) => {
-      emitValDef(sym, "\"<%s></%s>\"".format(name, name))
+    case Tag(name, children, attrs) => {
+      val attrsFormatted = (for ((name, value) <- attrs) yield {
+          // value is a list of string literals or symbols
+          val v = if (value.isEmpty) {
+            name
+          } else {
+            value.map { _ match {
+                case Const(lit) => lit.toString
+                case s: Sym[_] => s"$${${quote(s)}}"
+              }
+            }.mkString
+          }
+          s""" ${name}="${v}"""".format(name, v)
+        }).mkString
+      if (children.isEmpty) {
+        emitValDef(sym, "s\"\"\"<%s%s />\"\"\"".format(name, attrsFormatted))
+      } else {
+        val childrenFormatted = children.map(child => s"$${${quote(child)}}").mkString
+        emitValDef(sym, "s\"\"\"<%s%s>%s</%s>\"\"\"".format(name, attrsFormatted, childrenFormatted, name))
+      }
     }
     case _ => super.emitNode(sym, node)
   }
