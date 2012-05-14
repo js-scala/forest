b444d303a2f367eed6f16b544ad1251d2952d2ac
Julien Richard-Foy <julien@richard-foy.fr>
2012-05-14 16:44:12 +0200

    Minor changes in the compiler API

--- compiler/src/main/scala/forest/Run.scala
+++ compiler/src/main/scala/forest/Run.scala
@@ -1,16 +1,15 @@
 package forest
-import forest.backends._
+
 import scalax.file.Path
 
 object Run extends App {
-  
+
   if (args.size < 2) {
     sys.error("Usage: run <source> <target>")
   }
-  
-  val compiler = new Compiler
+
   val sourceDir = Path(new java.io.File(args(0)))
   val targetDir = Path(new java.io.File(args(1)))
-  
-  compiler.compile(sourceDir, targetDir)
+
+  Compiler.compile(sourceDir, targetDir)
 }
