88258ef1e2693873ffbbbc1a41569313c0bf2b04
Julien Richard-Foy <julien@richard-foy.fr>
2012-05-24 11:09:46 +0200

    cleaning

--- project/build.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-import Keys._
-
-object Forest extends Build {
-  lazy val compiler = Project(id = "forest-compiler", base = file("compiler"))
-  lazy val lmsRuntime = Project(id = "forest-lms", base = file("lms"))
-}
