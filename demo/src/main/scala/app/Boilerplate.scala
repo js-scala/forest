d9c3a0ea7c7f73fbe8f2a6896badda545ec0d39a
Julien Richard-Foy <julien@richard-foy.fr>
2012-05-31 09:57:26 +0200

    Reduce boilerplate

--- demo/src/main/scala/app/Boilerplate.scala
+++ demo/src/main/scala/app/Boilerplate.scala
@@ -6,21 +6,13 @@ import js._
 
 
 trait PersonOps extends Base {
-
-  implicit def repToPersonOps(p: Rep[Person]) = new PersonOps(p)
-
-  class PersonOps(p: Rep[Person]) {
-    def children: Rep[List[String]] = person_children(p)
-  }
-
-  def person_children(p: Rep[Person]): Rep[List[String]]
-
+  def infix_children(p: Rep[Person]): Rep[List[String]]
 }
 
 trait PersonOpsExp extends PersonOps with FieldsExp {
-  override def person_children(p: Exp[Person]): Exp[List[String]] = Field[Person, List[String]](p, "children")
+  override def infix_children(p: Exp[Person]): Exp[List[String]] = Field[Person, List[String]](p, "children")
 }
 
 trait PersonInScala extends PersonOps with JSInScala {
-  override def person_children(p: Person): List[String] = p.children
+  override def infix_children(p: Person): List[String] = p.children
 }
