079f3ea73a837493345970e7ff0f30a4fc528851
Julien Richard-Foy <julien@richard-foy.fr>
2012-07-11 14:34:40 +0200

    Add Scala XML backend

--- forest/src/main/scala/forest/lms/ScalaGenForest.scala
+++ forest/src/main/scala/forest/lms/ScalaGenForest.scala
@@ -8,7 +8,7 @@ import java.io.PrintWriter
  * Scala code generator for `ForestExp` expressions
  */
 // TODO I should extend a more general trait than ScalaGenEffect
-trait ScalaGenForest extends ScalaGenEffect with ScalaGenListOps2 { // this: ScalaGenListOps2 =>
+trait ScalaGenForest extends ScalaGenEffect {
   val IR: ForestExp with ListOps2Exp
   import IR._
 
@@ -55,6 +55,62 @@ trait ScalaGenForest extends ScalaGenEffect with ScalaGenListOps2 { // this: Sca
   }
 }
 
-trait ScalaGenForestPkg extends ScalaGenForest with ScalaGenProxy with ScalaGenModules {
+trait ScalaGenForestXml extends ScalaGenEffect {
+  val IR: ForestExp with ListOps2Exp
+  import IR._
+
+  override def emitNode(sym: Sym[Any], node: Def[Any])(implicit stream: PrintWriter): Unit = node match {
+    case Tag(name, children, attrs, _) => {
+      val attrsFormatted = (for ((name, value) <- attrs) yield {
+        // value is a list of string literals or symbols
+        if (value.isEmpty) {
+          " " + name + "='" + name + "'"
+        } else {
+          val v = value.map {
+            case Const(lit) => "\"" + lit.toString.replace("\"", "\\\"") + "\""
+            case s: Sym[_] => quote(s)
+          }.mkString(" + ")
+          " " + name + "={" + v + "}"
+        }
+      }).mkString(" ")
+      children match {
+        case Def(ConstList(xs)) if xs.isEmpty =>
+          emitValDef(sym, "<%s%s />".format(name, attrsFormatted))
+        case _ =>
+          emitValDef(sym, "<%s%s>{%s}</%s>".format(name, attrsFormatted, quote(children), name))
+      }
+    }
+
+    case Text(content) => {
+      emitValDef(sym, "{xml.Text(%s)}".format(content.map(quote).mkString(" + ")))
+    }
+
+    case Tree(root) => {
+      emitValDef(sym, quote(root))
+    }
+
+    case TreeRoot(tree) => {
+      emitValDef(sym, quote(tree))
+    }
+
+    case _ => super.emitNode(sym, node)
+
+  }
+
+  override def remap[A](m: Manifest[A]) = {
+    if (m == manifest[Tree]) {
+      "xml.Node"
+    } else {
+      super.remap(m)
+    }
+  }
+
+}
+
+trait ScalaGenForestPkg extends ScalaGenForest with ScalaGenProxy with ScalaGenModules with ScalaGenListOps2 {
+  val IR: ForestPkgExp
+}
+
+trait ScalaGenForestXmlPkg extends ScalaGenForestXml with ScalaGenProxy with ScalaGenModules with ScalaGenListOps2 {
   val IR: ForestPkgExp
 }
